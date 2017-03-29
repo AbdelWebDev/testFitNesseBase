8e4ebaa1b62bb0bdd9c13e8f9278acc2a3ec3965
AMRI <abdelouhab.amri@capgemini.com>
2017-03-29 16:10:58 +0200

    soap auotomation and adding dependencies for hsac on my pom

--- src/test/java/smartrics/rest/sample/RunnerTest.java
+++ src/test/java/smartrics/rest/sample/RunnerTest.java
@@ -1,23 +1,26 @@
 package smartrics.rest.sample;
 
-import java.io.File;
+import fitnesse.junit.JUnitHelper;
 import org.junit.Before;
 import org.junit.Test;
 
-import fitnesse.junit.JUnitHelper;
-
+import java.io.File;
 public class RunnerTest {
 
     private JUnitHelper helper;
 
     @Before
-    public void prepare() {
+    public void preparation() {
         helper = new JUnitHelper("C:\\", new File("target", "fitnesse-junit").getAbsolutePath());
         helper.setPort(9090);
     }
 
     @Test
-    public void restFixtureTests() throws Exception {
+    public void restFixtureAndHsacSoapXmlHttpTests() throws Exception {
         helper.assertSuitePasses("MyTestSamples.MySimple1");
+        helper.assertSuitePasses("MyTestSamples.MySimple2");
+        helper.assertSuitePasses("MyTestSamples.MySimple3");
+        helper.assertSuitePasses("MyTestSamples.MySimple4");
+        helper.assertSuitePasses("MyTestSamples.MySimple6");
     }
 }
